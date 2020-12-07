5e5a2b9817ac20804137d9d9260aa85546dd3ec7
xujiabin <xujiabin0114@163.com>
2020-12-07 11:28:09 +0800

    Initial commit

---
+++ poetryandyou/src/main/java/cn/yantumeijing/poetryandyou/PoetryandyouApplication.java
@@ -0,0 +1,13 @@
+package cn.yantumeijing.poetryandyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetryandyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetryandyouApplication.class, args);
+	}
+
+}
---
+++ poetryandyou/src/test/java/cn/yantumeijing/poetryandyou/PoetryandyouApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yantumeijing.poetryandyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetryandyouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
